0def15a5a4e86817e34f62958146b62f433c120f
Lars Kellogg-Stedman <lars@oddbit.com>
2022-10-07 08:27:47 -0400

    Update main.go to import subcommands
    Our new subcommands are invisible because we aren't importing them
    anywhere. In this commit, we import them into the top-level `main.go` file.
    Because we're only interested in the side-effects of the import (including
    the packages in our build so that the `init` functions will run), we need
    to use the blank identifier (`_`) for the imports. From [1]:
    
        To import a package solely for its side-effects (initialization), use
        the blank identifier as explicit package name:
    
        import _ "lib/math"
    
    [1]: https://go.dev/ref/spec#Import_declarations

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "example/cmd"
+import (
+	"example/cmd"
+	_ "example/cmd/bar"
+	_ "example/cmd/foo"
+)
 
 func main() {
 	cmd.Execute()
